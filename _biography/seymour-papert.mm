56441c4385f7703cdbaa4cf9b04bf7080f417266
evitse <41148265+evitse@users.noreply.github.com>
2018-12-16 21:10:02 +0200

    Create seymour-papert.mm

---
+++ _biography/seymour-papert.mm
@@ -0,0 +1,19 @@
+---
+author: evitse
+title: "Seymour Papert"
+image_url: /images/alan-kay.jpg
+caption: "Μαθηματικός και επιστήμονας πληροφορικής, γνωστός για τη συνεισφορά του στους τρόπους με τους οποίους η τεχνολογία μπορεί να υποστηρίξει τη μάθηση."
+categories: 
+  - biography
+  - definition
+tags:
+  - Alan Kay
+  - Seymour Papert
+  - Ivan Sutherland
+  - Bret Victor
+---
+
+{% include bio-papert.md %}
+
+{% include slides.html caption="Εικόνες για την βιογραφία του Σέημουρ Παπέρτ" %}
+
