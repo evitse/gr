cc23e51e75af867fcfa538215465981bcbd82b91
evitse <41148265+evitse@users.noreply.github.com>
2018-12-16 10:21:12 +0200

    Update and rename seymour-papert.md to seymour-papert.mm

---
+++ _biography/seymour-papert/seymour-papert.mm
@@ -0,0 +1,29 @@
+---
+author: evitse
+title: "Seymour Papert"
+image_url: /images/papert.jpg
+caption: " O Σέημουρ Παπέρτ γεννήθηκε στις 29 Φεβρουαρίου 1928 στην Πρετόρια της Νότιας Αφρικής και πέθανε στις 31 Ιουλίου 2016 στο Blue Hill του Maine, των ΗΠΑ. Μαθηματικός και επιστήμονας πληροφορικής, έγινε γνωστός για τη συνεισφορά του στην κατανόηση της διαδικασίας μάθησης των παιδιών και τους τρόπους με τους οποίους η τεχνολογία μπορεί να υποστηρίξει τη μάθηση."
+categories:
+  - biography
+  - definition
+tags:
+  - Alan Kay
+  - Seymour Papert
+  - Ivan Sutherland
+  - Bret Victor
+  - Dynabook
+  - Smalltalk
+  - Xerox Alto
+gallery:
+  - image_url: /images/papert.jpg
+    caption: "Το Dynabook είναι ένα σχεδιαστικό όραμα του 1968 για ένα φορητό υπολογιστή τύπου τάμπλετ από τον Alan Kay που απευθύνεται σε παιδιά και μπορεί να προγραμματιστεί με στόχο την προσωπική έκφραση και την επεξεργασία της πληροφορίας"
+  - image_url: /images/papert.jpg
+    caption: "Alan Kay in front of a Xerox Alto and a piano"
+  - image_url: /images/smalltalk.jpg
+    caption: 'Η αντικειμενοστραφής γλώσσα προγραμματισμού Smalltalk είχε έμφαση σε οντότητες υψηλού επιπέδου και στην διάδραση με τον χρήστη και έτσι διευκόλυνε την κατασκευή και τις δοκιμές του λογισμικού που τελικά οδήγησε στους πρώτους επιτυχημένους εμπορικά επιτραπέζιους υπολογιστές'
+---
+
+{% include_relative bio-papert.md %}
+
+{% include slides.html caption="Εικόνες για την βιογραφία του Σέημουρ Παπέρτ" %}
+
