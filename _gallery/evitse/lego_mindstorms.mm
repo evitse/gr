315ad98714a2baf10327a0aad260e8eaa652afbd
evitse <41148265+evitse@users.noreply.github.com>
2018-12-22 20:44:06 +0200

    Update lego_mindstorms.mm

--- _gallery/evitse/lego_mindstorms.mm
+++ _gallery/evitse/lego_mindstorms.mm
@@ -1,7 +1,7 @@
 ---
 author: evitse
 title: Lego Mindstorms
-image_url: /images/Easypascal.jpg
+image_url: /images/1024px-Jugend_Hackt_Ost_2016_-_Lego_Mindstorms_wurde_viel_verwendet.jpg
 caption: 'Παρόλο που υπάρχουν ορισμένες εκδόσεις της  Logo με compiler, γενικά εφαρμόζεται ως interpreted  γλώσσα. Η αλληλεπίδραση αυτής της προσέγγισης παρέχει στον χρήστη άμεση ανατροφοδότηση σχετικά με τις μεμονωμένες οδηγίες, βοηθώντας έτσι στη διαδικασία debugging και την εκμάθηση.
 '
 license_url: 'https://commons.wikimedia.org/wiki/File:Easypascal.jpg'
