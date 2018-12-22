4e07a9be1136eb87ca1a01f54518f6cab5100edd
evitse <41148265+evitse@users.noreply.github.com>
2018-12-22 20:51:14 +0200

    Update lego_mindstorms.mm

--- _gallery/evitse/lego_mindstorms.mm
+++ _gallery/evitse/lego_mindstorms.mm
@@ -5,7 +5,7 @@ image_url: /images/1024px-Jugend_Hackt_Ost_2016_-_Lego_Mindstorms_wurde_viel_ver
 caption: 'Παρόλο που υπάρχουν ορισμένες εκδόσεις της  Logo με compiler, γενικά εφαρμόζεται ως interpreted  γλώσσα. Η αλληλεπίδραση αυτής της προσέγγισης παρέχει στον χρήστη άμεση ανατροφοδότηση σχετικά με τις μεμονωμένες οδηγίες, βοηθώντας έτσι στη διαδικασία debugging και την εκμάθηση.
 '
 license_url: 'https://commons.wikimedia.org/wiki/File:Easypascal.jpg'
-license_text: Easypascal of Wikimedia Commons
+license_text: Lego Mindstorms wurde viel verwendet of Wikimedia Commons
 categories:
   - method
   - case-study
