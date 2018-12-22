0deac86d3f1d5dc7c6dbca810a1b18c080298053
evitse <41148265+evitse@users.noreply.github.com>
2018-12-22 20:37:14 +0200

    Update logo.mm

--- _gallery/evitse/logo.mm
+++ _gallery/evitse/logo.mm
@@ -4,16 +4,14 @@ title: Logo
 image_url: /images/Cpc464.computer.750pix.jpg
 caption: 'Παρόλο που υπάρχουν ορισμένες εκδόσεις της  Logo με compiler, γενικά εφαρμόζεται ως interpreted  γλώσσα. Η αλληλεπίδραση αυτής της προσέγγισης παρέχει στον χρήστη άμεση ανατροφοδότηση σχετικά με τις μεμονωμένες οδηγίες, βοηθώντας έτσι στη διαδικασία debugging και την εκμάθηση
 '
-license_url: 'https://commons.wikimedia.org/wiki/File:Cpc464.computer.750pix.jpg'
-license_text: Arpingstone of Wikimedia Commons
+license_url: 'https://commons.wikimedia.org/wiki/File:Papert-moscow-1987.jpg'
+license_text: Shen-montpellier of Wikimedia Commons
 categories:
   - method
   - case-study
 tags:
-  - MS-DOS
-  - Microsoft
-  - Windows
-  - OS
+  - Seymour Papert
+  - Logo
 ---
 
 
