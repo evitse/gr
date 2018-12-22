86c71946d2a17c66d08b5a7a53fa790a927619f5
evitse <41148265+evitse@users.noreply.github.com>
2018-12-22 21:05:28 +0200

    Update lisp.mm

--- _gallery/evitse/lisp.mm
+++ _gallery/evitse/lisp.mm
@@ -1,1 +1,16 @@
+---
+author: evitse
+title: Logo
+image_url: /images/Cpc464.computer.750pix.jpg
+caption: 'Παρόλο που υπάρχουν ορισμένες εκδόσεις της  Logo με compiler, γενικά εφαρμόζεται ως interpreted  γλώσσα. Η αλληλεπίδραση αυτής της προσέγγισης παρέχει στον χρήστη άμεση ανατροφοδότηση σχετικά με τις μεμονωμένες οδηγίες, βοηθώντας έτσι στη διαδικασία debugging και την εκμάθηση
+'
+license_url: 'https://commons.wikimedia.org/wiki/File:Papert-moscow-1987.jpg'
+license_text: Shen-montpellier of Wikimedia Commons
+categories:
+  - method
+  - case-study
+tags:
+  - Seymour Papert
+  - Logo
+---
 
