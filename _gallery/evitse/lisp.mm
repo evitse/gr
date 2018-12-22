2d242d22e2f081894833682892b6fadea90bc767
evitse <41148265+evitse@users.noreply.github.com>
2018-12-22 21:07:55 +0200

    Update lisp.mm

--- _gallery/evitse/lisp.mm
+++ _gallery/evitse/lisp.mm
@@ -1,7 +1,7 @@
 ---
 author: evitse
-title: Logo
-image_url: /images/Cpc464.computer.750pix.jpg
+title: Lisp
+image_url: /images/580px-LISP_machine.jpg
 caption: 'Παρόλο που υπάρχουν ορισμένες εκδόσεις της  Logo με compiler, γενικά εφαρμόζεται ως interpreted  γλώσσα. Η αλληλεπίδραση αυτής της προσέγγισης παρέχει στον χρήστη άμεση ανατροφοδότηση σχετικά με τις μεμονωμένες οδηγίες, βοηθώντας έτσι στη διαδικασία debugging και την εκμάθηση
 '
 license_url: 'https://commons.wikimedia.org/wiki/File:Papert-moscow-1987.jpg'
